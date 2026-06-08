91fef8988d8b2cef5398cc3f0bbdda2bae168a85
Jackie Macharia <16131515+jackiewmacharia@users.noreply.github.com>
2026-06-08 14:32:50 +0200

    update general aliases (#8)

---
+++ zsh/aliases/general.zsh
@@ -0,0 +1,18 @@
+# navigation
+alias home='~'
+alias desktop='~/Desktop'
+alias mkdir='mkdir -p'
+mkdircd() { mkdir -p "$1" && "$1"; }
+
+# file operations
+alias cp='cp -i'
+alias mv='mv -i'
+alias ll='ls -lh'
+alias lsa='ls -la'
+alias grep='grep --color=auto'
+
+# shell
+alias c='clear'
+alias h='history'
+alias reload='source ~/.zshrc'
+alias path='echo $PATH | tr ":" "\n"'
