60f75ab37336c372fdb2f42bc359b42167b8cdd3
Jackie Macharia <16131515+jackiewmacharia@users.noreply.github.com>
2026-06-11 17:28:19 +0200

    Delete neovim/after/plugin/after/plugin/treesitter.lua

--- neovim/after/plugin/after/plugin/treesitter.lua
+++
@@ -1,5 +0,0 @@
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { "lua", "vim", "vimdoc", "python", "javascript", "typescript", "bash", "markdown", "markdown_inline" },
-  auto_install = true,
-  highlight = { enable = true },
-})
